52c534e6cb7efe2df66f2ef5698ee3276fec052f
shotaseanyasuda <77286593+shotaseanyasuda@users.noreply.github.com>
2021-11-29 19:48:04 -0500

    Hello message test

--- app/src/main/java/com/example/blackjack_final_project/MainActivity.java
+++ app/src/main/java/com/example/blackjack_final_project/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Hello this is a test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
