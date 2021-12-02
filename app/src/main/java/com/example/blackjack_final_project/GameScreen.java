09c499d4a24c86ea2529508e07d1d915a9222de1
Jmart436 <77401140+Jmart436@users.noreply.github.com>
2021-12-02 14:17:47 -0500

    some functionality to "Stand" button

--- app/src/main/java/com/example/blackjack_final_project/GameScreen.java
+++ app/src/main/java/com/example/blackjack_final_project/GameScreen.java
@@ -40,6 +40,7 @@ public class GameScreen extends AppCompatActivity {
     public TextView seekBarTextView;
     public Button endGameButton; // end game button
     public Button dealButton; // deal button
+    public Button standButton; // stand button
     public SeekBar betSeekbar; // seekbar
     public String bankAmountTotalString = MainActivity.bankAmountTotalString;
     public double customBet; // current bet total used for calculations
@@ -49,7 +50,6 @@ public class GameScreen extends AppCompatActivity {
     public ImageView playerCard2;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,10 +57,11 @@ public class GameScreen extends AppCompatActivity {
 
         dealerTotal = (TextView) findViewById(R.id.dealer_total_textview);
         playerTotal = (TextView) findViewById(R.id.user_total_textview);
-        betTextView = (TextView)findViewById(R.id.bet_textview);
+        betTextView = (TextView) findViewById(R.id.bet_textview);
         seekBarTextView = (TextView) findViewById(R.id.seek_bar_textview);
         endGameButton = (Button) findViewById(R.id.end_game_button);
         dealButton = (Button) findViewById(R.id.deal_button);
+        standButton = (Button) findViewById(R.id.stand_button);
         betSeekbar = (SeekBar) findViewById(R.id.bet_amount_seekbar);
         betSeekbar.setOnSeekBarChangeListener(betSeekbarListener);
 
@@ -74,15 +75,28 @@ public class GameScreen extends AppCompatActivity {
         dealButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dealerCard2.setImageResource(R.drawable.cardback); // sets dealer card to the back of the card
+
                 playCounter += 1; // adds one to play counter
-                dealCards(); // calls deal cards
+                dealCardsP1(); // calls deal cards player card 1
+                dealCardsP2(); // calls deal cards player card 2
+                dealCardD1(); //calls deal cards dealer card 2
                 updatePlayerTotal(); // updates player totals
                 dealButton.setVisibility(View.INVISIBLE); // removed deal button
                 betSeekbar.setVisibility(View.INVISIBLE); // removes bet seekbar
                 seekBarTextView.setVisibility(View.INVISIBLE); // removes "place your bet" text
             }// end on click of deal button
         });// end override
+
+        standButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dealCardsD2();
+                updatePlayerTotal();
+            }// end on click
+        });
+
+
+
 
         endGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,22 +116,25 @@ public class GameScreen extends AppCompatActivity {
             customBet = progress; // sets customBet to value of seekbar
             updateBet(); // updates the current bet amount
         }
+
         @Override
         public void onStartTrackingTouch(SeekBar seekBar) {
         }
+
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) {
         }
     };
 
     //method to deal cards
-    public void dealCards(){
+    public void dealCardD1() {
         // dealer card 1
+        dealerCard2.setImageResource(R.drawable.cardback); // sets dealer card to the back of the card
         Random randomSuit = new Random();
         Random randomValue = new Random();
-        cardSuit = 1 + randomSuit.nextInt(5-1);
-        cardValue = 1+ randomValue.nextInt(14-1);
-        switch (cardSuit){
+        cardSuit = 1 + randomSuit.nextInt(5 - 1);
+        cardValue = 1 + randomValue.nextInt(14 - 1);
+        switch (cardSuit) {
             case 1:
                 suitConversion = "c";
                 break;
@@ -132,14 +149,16 @@ public class GameScreen extends AppCompatActivity {
                 break;
         }// end switch
         changeThisName = suitConversion + cardValue; // creates a string to access image resouce file
-        dealerCard1.setImageResource(getResources().getIdentifier(changeThisName,"drawable",getPackageName())); // sets image for dealer card 1
+        dealerCard1.setImageResource(getResources().getIdentifier(changeThisName, "drawable", getPackageName())); // sets image for dealer card 1
+    }// end deal D1
+    public void dealCardsD2(){
         // dealer card 2
-        if (playCounter > 1){
+
             Random randomSuit2 = new Random();
             Random randomValue2 = new Random();
-            cardSuitD2 = 1 + randomSuit2.nextInt(5-1);
-            cardValueD2 = 1+ randomValue2.nextInt(14-1);
-            switch (cardSuitD2){
+            cardSuitD2 = 1 + randomSuit2.nextInt(5 - 1);
+            cardValueD2 = 1 + randomValue2.nextInt(14 - 1);
+            switch (cardSuitD2) {
                 case 1:
                     suitConversion = "c";
                     break;
@@ -154,14 +173,17 @@ public class GameScreen extends AppCompatActivity {
                     break;
             }// end switch
             changeThisName = suitConversion + cardValueD2;
-            dealerCard2.setImageResource(getResources().getIdentifier(changeThisName,"drawable",getPackageName()));
-        }// end if player count > 1
+            dealerCard2.setImageResource(getResources().getIdentifier(changeThisName, "drawable", getPackageName()));
+
+    }// end deal cards D2
+
+    public void dealCardsP1() {
         //player card 1
         Random randomSuitP1 = new Random();
         Random randomValueP1 = new Random();
-        cardSuitP1 = 1 + randomSuitP1.nextInt(5-1);
-        cardValueP1 = 1+ randomValueP1.nextInt(14-1);
-        switch (cardSuitP1){
+        cardSuitP1 = 1 + randomSuitP1.nextInt(5 - 1);
+        cardValueP1 = 1 + randomValueP1.nextInt(14 - 1);
+        switch (cardSuitP1) {
             case 1:
                 suitConversion = "c";
                 break;
@@ -176,13 +198,16 @@ public class GameScreen extends AppCompatActivity {
                 break;
         }// end switch
         changeThisName = suitConversion + cardValueP1; // creates a string to access image resouce file
-        playerCard1.setImageResource(getResources().getIdentifier(changeThisName,"drawable",getPackageName())); // sets image for dealer card 1
+        playerCard1.setImageResource(getResources().getIdentifier(changeThisName, "drawable", getPackageName())); // sets image for dealer card 1
+    }// end deal P1
+
+    public void dealCardsP2() {
         // player card 2
         Random randomSuitP2 = new Random();
         Random randomValueP2 = new Random();
-        cardSuitP2 = 1 + randomSuitP2.nextInt(5-1);
-        cardValueP2 = 1+ randomValueP2.nextInt(14-1);
-        switch (cardSuitP2){
+        cardSuitP2 = 1 + randomSuitP2.nextInt(5 - 1);
+        cardValueP2 = 1 + randomValueP2.nextInt(14 - 1);
+        switch (cardSuitP2) {
             case 1:
                 suitConversion = "c";
                 break;
@@ -197,8 +222,9 @@ public class GameScreen extends AppCompatActivity {
                 break;
         }// end switch
         changeThisName = suitConversion + cardValueP2; // creates a string to access image resouce file
-        playerCard2.setImageResource(getResources().getIdentifier(changeThisName,"drawable",getPackageName())); // sets image for dealer card 1
-    }// end deal cards
+        playerCard2.setImageResource(getResources().getIdentifier(changeThisName, "drawable", getPackageName())); // sets image for dealer card 1
+    }// end deal cards P2
+
 
     public void updateBet(){// updates current bet amount
        betTextView.setText("Bet: " + String.valueOf(customBet));
