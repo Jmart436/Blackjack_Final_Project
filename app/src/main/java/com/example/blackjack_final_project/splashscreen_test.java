606e18724f04ffc4065a6af37a9f40fedd463d0f
MairKeen <69979556+MairKeen@users.noreply.github.com>
2021-12-03 13:51:11 -0500

    code for splash screen

--- app/src/main/java/com/example/blackjack_final_project/splashscreen_test.java
+++ app/src/main/java/com/example/blackjack_final_project/splashscreen_test.java
@@ -1,4 +1,25 @@
 package com.example.blackjack_final_project;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class splashscreen_test {
-}
+    /*public class MainActivity extends AppCompatActivity {
+        private static int Splash_time_out = 4000;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(homeIntent);
+                    finish();
+                }
+            },Splash_time_out);
+        }
+    */}
